bef35d8aecd1247eca6abd3af9e9c07e2ff56782
Adish Ansari <adishansari786@gmail.com>
2022-05-14 07:19:29 -0700

    Update Product.java

--- src/main/java/com/product/model/Product.java
+++ src/main/java/com/product/model/Product.java
@@ -21,6 +21,7 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
+    private String company;
     private int quantity;
     private double price;
     
@@ -48,12 +49,28 @@ public class Product {
 	public void setPrice(double price) {
 		this.price = price;
 	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
 	@Override
 	public String toString() {
-		return "Product [id=" + id + ", name=" + name + ", quantity=" + quantity + ", price=" + price + "]";
+		return "Product [id=" + id + ", name=" + name + ", company=" + company + ", quantity=" + quantity + ", price="
+				+ price + "]";
 	}
-    
-    
-    
-    
+	public Product(int id, String name, String company, int quantity, double price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.company = company;
+		this.quantity = quantity;
+		this.price = price;
+	}
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	    
 }
